8dd7c98e630b2a6fae8ef7efa60e512192d309c4
niravjdn <niravjdn@gmail.com>
2019-11-06 12:41:20 -0500

    Stage

--- Vehicle Rental System/src/main/java/com/sdmproject/configuration/SuccessHandler.java
+++ Vehicle Rental System/src/main/java/com/sdmproject/configuration/SuccessHandler.java
@@ -18,76 +18,70 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 
 import com.sdmproject.repository.UserRepository;
 
-public class SuccessHandler
-implements AuthenticationSuccessHandler {
+public class SuccessHandler implements AuthenticationSuccessHandler {
 
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
-  private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	Logger logger = LoggerFactory.getLogger(UserRepository.class);
 
-  Logger logger = LoggerFactory.getLogger(UserRepository.class);
-  
-  @Override
-  public void onAuthenticationSuccess(HttpServletRequest request, 
-    HttpServletResponse response, Authentication authentication)
-    throws IOException {
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException {
 
-      handle(request, response, authentication);
-      clearAuthenticationAttributes(request);
-  }
+		handle(request, response, authentication);
+		clearAuthenticationAttributes(request);
+	}
 
-  protected void handle(HttpServletRequest request, 
-    HttpServletResponse response, Authentication authentication)
-    throws IOException {
+	protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+			throws IOException {
 
-      String targetUrl = determineTargetUrl(authentication);
+		String targetUrl = determineTargetUrl(authentication);
 
-      if (response.isCommitted()) {
-          logger.debug(
-            "Response has already been committed. Unable to redirect to "
-            + targetUrl);
-          return;
-      }
+		if (response.isCommitted()) {
+			logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+			return;
+		}
 
-      redirectStrategy.sendRedirect(request, response, targetUrl);
-  }
+		redirectStrategy.sendRedirect(request, response, targetUrl);
+	}
 
-  protected String determineTargetUrl(Authentication authentication) {
-      boolean isUser = false;
-      boolean isAdmin = false;
-      Collection<? extends GrantedAuthority> authorities
-       = authentication.getAuthorities();
-      System.out.println(authorities.toString());
-      for (GrantedAuthority grantedAuthority : authorities) {
-          if (grantedAuthority.getAuthority().equals("CLERK")) {
-              isUser = true;
-              break;
-          } else if (grantedAuthority.getAuthority().equals("ADMIN")) {
-              isAdmin = true;
-              break;
-          }
-      }
+	protected String determineTargetUrl(Authentication authentication) {
+		boolean isUser = false;
+		boolean isAdmin = false;
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		System.out.println(authorities.toString());
+		for (GrantedAuthority grantedAuthority : authorities) {
+			if (grantedAuthority.getAuthority().equals("CLERK")) {
+				isUser = true;
+				break;
+			} else if (grantedAuthority.getAuthority().equals("ADMIN")) {
+				isAdmin = true;
+				break;
+			}
+		}
 
-      if (isUser) {
-          return "/clerk/home";
-      } else if (isAdmin) {
-    	  return "/admin/dashboard";
-      } else {
-          throw new IllegalStateException();
-      }
-  }
+		if (isUser) {
+			return "/clerk/home";
+		} else if (isAdmin) {
+			return "/admin/dashboard";
+		} else {
+			throw new IllegalStateException();
+		}
+	}
 
-  protected void clearAuthenticationAttributes(HttpServletRequest request) {
-      HttpSession session = request.getSession(false);
-      if (session == null) {
-          return;
-      }
-      session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-  }
+	protected void clearAuthenticationAttributes(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			return;
+		}
+		session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+	}
 
-  public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
-      this.redirectStrategy = redirectStrategy;
-  }
-  protected RedirectStrategy getRedirectStrategy() {
-      return redirectStrategy;
-  }
+	public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+		this.redirectStrategy = redirectStrategy;
+	}
+
+	protected RedirectStrategy getRedirectStrategy() {
+		return redirectStrategy;
+	}
 }
